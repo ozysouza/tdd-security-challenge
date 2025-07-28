2d3a6e62a6f27b9fdfb106381c2701617ebe9c23
ozysouza <ozysouzaa@gmail.com>
2025-07-27 20:21:09 -0400

    Add seed and Integration tests for TDD

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_city(name) VALUES ('Toronto');
+INSERT INTO tb_city(name) VALUES ('Vancouver');
+INSERT INTO tb_city(name) VALUES ('Montreal');
+INSERT INTO tb_city(name) VALUES ('Calgary');
+INSERT INTO tb_city(name) VALUES ('Ottawa');
+INSERT INTO tb_city(name) VALUES ('Edmonton');
+INSERT INTO tb_city(name) VALUES ('Winnipeg');
+INSERT INTO tb_city(name) VALUES ('Halifax');
+INSERT INTO tb_city(name) VALUES ('Quebec City');
+INSERT INTO tb_city(name) VALUES ('Victoria');
+INSERT INTO tb_city(name) VALUES ('Hamilton');
+
+INSERT INTO tb_event(name, date, url, city_id) VALUES ('Software Fair', '2021-05-16', 'https://softwarefair.ca', 1);
+INSERT INTO tb_event(name, date, url, city_id) VALUES ('Canadian Comic Expo', '2021-04-13', 'https://cancomicexpo.ca', 1);
+INSERT INTO tb_event(name, date, url, city_id) VALUES ('Linux Conference Canada', '2021-05-23', 'https://linuxconf.ca', 2);
+INSERT INTO tb_event(name, date, url, city_id) VALUES ('Spring React Week', '2021-05-03', 'https://devsuperior.ca', 3);
