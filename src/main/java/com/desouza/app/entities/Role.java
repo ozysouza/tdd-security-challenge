3d624afa46a8451db742d79c1ff05bb875dfa1d0
ozysouza <ozysouzaa@gmail.com>
2025-07-29 22:00:48 -0400

    Remove setId for User and Role

--- src/main/java/com/desouza/app/entities/Role.java
+++ src/main/java/com/desouza/app/entities/Role.java
@@ -29,10 +29,6 @@ public class Role {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getAuthority() {
         return authority;
     }
