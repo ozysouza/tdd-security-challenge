7ab3726ff6566776a4b1644e4d8afb8f387ddd0d
ozysouza <ozysouzaa@gmail.com>
2025-07-29 22:07:19 -0400

    Add seed for Users and Roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,3 +14,12 @@ INSERT INTO tb_event(name, date, url, city_id) VALUES ('Software Fair', '2021-05
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Canadian Comic Expo', '2021-04-13', 'https://cancomicexpo.ca', 1);
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Linux Conference Canada', '2021-05-23', 'https://linuxconf.ca', 2);
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Spring React Week', '2021-05-03', 'https://devsuperior.ca', 3);
+
+INSERT INTO tb_user (email, password) VALUES ('ana@gmail.com', '$2a$10$hekAig9V0rG/0/Svd5m6..7sl3hQmVhBwqaNmbf9zfEsj6brViBfC');
+INSERT INTO tb_user (email, password) VALUES ('bob@gmail.com', '$2a$10$hekAig9V0rG/0/Svd5m6..7sl3hQmVhBwqaNmbf9zfEsj6brViBfC');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
