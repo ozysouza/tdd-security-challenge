5199f1cd1a39bba870a0ff848e3622f696eb8b17
ozysouza <ozysouzaa@gmail.com>
2025-07-31 23:40:23 -0400

    Add methos to cover tests insertShouldReturn403WhenClientLogged and insertShouldInsertResourceWhenAdminLoggedAndCorrectData

--- src/main/java/com/desouza/app/controllers/CityController.java
+++ src/main/java/com/desouza/app/controllers/CityController.java
@@ -1,17 +1,24 @@
 package com.desouza.app.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.desouza.app.dto.CityDTO;
 import com.desouza.app.service.CityService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/cities")
@@ -19,6 +26,18 @@ public class CityController {
 
     @Autowired
     private CityService cityService;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto) {
+        dto = cityService.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
 
     @GetMapping
     public ResponseEntity<List<CityDTO>> findAll(@PageableDefault(sort = "name") Pageable pageable) {
